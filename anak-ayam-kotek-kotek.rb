5dee5a7bfb6df945ccfa069f36fd624f3b7d202c
gerry leo nugroho <gerryleonugroho@gmail.com>
2015-06-09 17:35:58 +0700

    ruby file edited

--- anak-ayam-kotek-kotek.rb
+++ anak-ayam-kotek-kotek.rb
@@ -1,13 +1,15 @@
 # Ask Them How Many Anak Ayam Will Get Counted Down!
-puts 'Hi, welcome this is just a simple software to demonstrate how ruby' 
-puts 'really that good to do math. So, how many chicken should I chomp today?'
-puts 'Write a number below than press, "Enter".'
+puts 'Hi there, and welcome, this is just a simple software to demonstrate how ruby' 
+puts 'really that good to do some math. So, how many chicken should I chomp today?'
+puts 'Now, write down a number below than press, "Enter".'
+
 anak_ayam = gets.chomp    
 if anak_ayam.is_a?(Integer) == true
   puts ''
 else
-  puts ''
-# Now Get Down With It!
+  if anak_ayam.is_a?(Integer) == false
+  puts 'Please write down numeric numbers, eg (1,10,100)'
+# Now get down with it!
 while anak_ayam != 0
   puts 'Tek Kotek Kotek Kotek'
   puts 'Anak ayam turun '+anak_ayam.to_s+'.'
@@ -15,9 +17,8 @@ while anak_ayam != 0
   anak_ayam = anak_ayam.to_i - 1  
   puts 'Mati satu tinggalah '+anak_ayam.to_s+''
   puts ''
-# If We Have One Anak Ayam Running Left!
+# If we have one anak ayam running left!
   if anak_ayam == 1            
-    puts ''
     puts 'Tek Kotek Kotek Kotek' 
     puts 'Anak ayam turunlah '+anak_ayam.to_s+'.'
     anak_ayam = anak_ayam - 1      
@@ -25,6 +26,7 @@ while anak_ayam != 0
     puts 'Mati satu tinggalah '+anak_ayam.to_s+''          
   else                     
   end
+end
 end
 
 puts ''                  
