13ebd8d29232b9b580a3bd901d5aeebb6eaaaac2
gerry leo nugroho <gerryleonugroho@gmail.com>
2015-06-14 07:23:47 +0700

    another class to restore

--- anak-ayam-kotek-kotek.rb
+++ anak-ayam-kotek-kotek.rb
@@ -1,6 +1,8 @@
-localVar = "anak_ayam"
-
-
+class AnakAyam
+  def tekKotek( aName )
+    @myname = aName
+  end 
+  
 # Ask Them How Many Anak Ayam Will Get Counted Down!
 puts 'Hi there, and welcome, this is just a simple software to demonstrate how ruby' 
 puts 'really that good to do some math. So, how many chicken should I chomp today?'
@@ -10,11 +12,10 @@ print('Enter the chicken : ' )
 anak_ayam = gets.chomp    
 if anak_ayam.is_a?(Integer) == true then 
   puts '\n'
-    else
+    else 
       if anak_ayam.is_a?(Integer) == false then
-        puts 'Please write down numeric numbers, eg (1,10,100)'
+        puts 'Please write down numeric numbers, eg (1,10,100)'  
         # Now get down with it, and chop down the chicken!
-        puts ''
         while anak_ayam != 0
             puts 'Tek Kotek Kotek Kotek'
             puts 'Anak ayam turun '+anak_ayam.to_s+'.'
@@ -35,4 +36,6 @@ if anak_ayam.is_a?(Integer) == true then
 
 puts ''
 end
+
+
 
